2fa0204b6ef83873251a1b2fd29ecc849b535192
ecosnier2017 <elie.cosnier2017@campus-eni.fr>
2018-11-26 15:34:10 +0100

    develop : fix use

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -7,8 +7,9 @@ use App\Form\UtilisateurType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UtilisateurController extends Controller
 {
